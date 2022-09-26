360e041a08a12f4373857d7a213287c2bd16a585
shielapuyan <shiela.puyan@hva.nl>
2022-09-26 16:09:57 +0200

    3.3.1 A af

---
+++ auc-vue/src/App33.vue
@@ -0,0 +1,23 @@
+<template>
+  <HeaderVue></HeaderVue>
+  <NavbarVue></NavbarVue>
+  <router-view></router-view>
+</template>
+
+<script>
+
+import HeaderVue from './components/Header.vue'
+import NavbarVue from './components/Navbar.vue'
+
+export default {
+  name: "App33.vue",
+  components: {
+    HeaderVue, NavbarVue
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
