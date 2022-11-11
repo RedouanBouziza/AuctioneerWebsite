a0ce47cc939ee2753cf4d90a79abbc6b4b5a7f11
shielapuyan <shiela.puyan@hva.nl>
2022-11-11 01:47:34 +0100

    fix

--- auc-server/src/main/java/app/repositories/OffersRepository.java
+++ auc-server/src/main/java/app/repositories/OffersRepository.java
@@ -1,9 +1,11 @@
 package app.repositories;
 
 import app.models.Offer;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OffersRepository {
     public List<Offer> findAll();
 }
--- auc-server/src/main/java/app/rest/OffersController.java
+++ auc-server/src/main/java/app/rest/OffersController.java
@@ -3,10 +3,14 @@ package app.rest;
 import app.models.Offer;
 import app.repositories.OffersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
+@RestController
 public class OffersController {
 
     @Autowired
