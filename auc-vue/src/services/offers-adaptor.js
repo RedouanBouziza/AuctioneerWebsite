1556fad4fb36b7cfdf272e572d89a63bbf09eb84
shielapuyan <shiela.puyan@hva.nl>
2022-11-18 14:15:01 +0100

    update

---
+++ auc-vue/src/services/offers-adaptor.js
@@ -0,0 +1,36 @@
+import {Offer} from "@/models/offer";
+
+export class OffersAdaptor {
+    resourceUrl;
+    constructor(resourceUrl) {
+        this.resourceUrl = resourceUrl;
+        console.log("Created OffersAdaptor for " + resourceUrl);
+    }
+
+    async fetchJson (url, options = null){
+        let response = await fetch(url, options)
+        if(response.ok){
+            return await response.json();
+        } else {
+            console.log(response, !response.bodyUsed ? await response.text() : "");
+            return null;
+        }
+    }
+
+    async asyncFindAll(){
+        const offers = await this.fetchJson(this.resourcesUrl);
+        return offers?.map(s => Offer.copyConstructor(s));
+    }
+
+    async asyncFindById(id){
+        const offer = await this.fetchJson(this.resourcesUrl + "/" + id, 'GET')
+        return Offer.copyConstructor(offer)
+    }
+
+    async asyncSave(offer){
+        const offerParam = await this.fetchJson(this.resourcesUrl + "/" + offer.id, 'POST', offer)
+    }
+    async asyncDeleteById(id){
+        return await this.fetchJson(this.resourcesUrl + "/" + id, 'DELETE')
+    }
+}
